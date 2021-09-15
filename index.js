2764c1fe556288d30d5bf9e4b9c639df2c244b5f
ezimon <simonespeche123@gmail.com>
2021-09-15 16:44:12 -0300

    scnd commite esta

--- index.js
+++ index.js
@@ -1,28 +1,73 @@
 const express = require("express");
-const {google} = require("googleapis");
+const { google } = require("googleapis");
+
 const app = express();
 
-app.get("/", async (req, res) => {
+app.set('views', './views');
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-    const auth = new google.auth.googleAuth({
-        keyFile: "credentials.json",
+app.get("/", (req, res) => {
+    res.render('index')
+});
+
+app.post("/", async (req, res) => {
+    const { request, name } = req.body;
+    res.json('CARGADOOO')
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "cred.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
 
+    // Create client instance for auth
     const client = await auth.getClient();
+
+    // Instance of Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: client });
 
     const spreadsheetId = "1mUhl_FfdbIVPv48MBp0ZLqkBTXcwCKhCxsREMnE8PqY";
 
-    const googleSheets = google.sheets({version: "v4", auth: client});
-
+    // Get metadata about spreadsheet
     const metaData = await googleSheets.spreadsheets.get({
         auth,
         spreadsheetId,
-    })
+    });
 
-    res.send(metaData);
+    // Read rows from spreadsheet
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "pito!A:A",
+    });
 
+    // Write row(s) to spreadsheet
+    await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId,
+        range: "pito!A:B",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [['Concepto', 'Egreso']],
+        },
+    });
 
+    res.send("Successfully submitted! Thank you!");
 });
 
-app.listen(1337, (req, res) => console.log("runnin"));
+
+app.post("/submit", async (req, res) => {
+    // const { monto } = req.body;
+    await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId,
+        range: "pito!A:B",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [['monto', 'Egreso']],
+        }
+    })
+    res.json('OLA BB LO CARGASTE')
+});
+
+app.listen(1337, (req, res) => console.log("running on 1337"));
