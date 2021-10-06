01703f7b005382dd0aee27a6b634000dd07c7046
Simon <simonespeche123@gmail.com>
2021-10-06 00:29:11 -0300

    .

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ const { google } = require("googleapis");
 const app = express();
 const cors = require("cors");
 const moment = require("moment");
+// const bodyParser = require('body-parser');
 
-
-// CONSTANTES DEFINIDAS
+// CONSTANTES DE GOOGLE DEFINIDAS
 const auth = new google.auth.GoogleAuth({
     keyFile: "cred.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
 });
-
-// Create client instance for auth
 const client = auth.getClient();
-
-// Instance of Google Sheets API
 const googleSheets = google.sheets({ version: "v4", auth: client });
-
 const spreadsheetId = "1mUhl_FfdbIVPv48MBp0ZLqkBTXcwCKhCxsREMnE8PqY";
 
 app.set('views', './views');
@@ -24,24 +19,59 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/views'));
 app.use(cors());
-
+app.use(express.json())
+// app.use(bodyParser.json);
 
 app.get("/", (req, res) => {
     res.json('wsup');
 });
 
 
-app.post("/submit", async (req, res) => {
-    let { monto, tipo, paga, tipoPers } = req.body;
-    const fecha = new Date();
+app.get("/saldo", (apiUrl, res, saldo) => {
     const sheetname = moment().format('MMMM');
+    googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: sheetname + '!G1',
+    }, (err, result) => {
+        if (err) {
+            console.log('err')
+        } else {
+            const saldo = result.data.values[0].join('');
+            res.json(saldo)
+        }
+    });
+});
+
+
+// app.get("/prueba", async(req, res) => {
+//     const sheetname = moment().format('MMMM');
+//     googleSheets.spreadsheets.values.get({
+//         auth,
+//         spreadsheetId,
+//         range: sheetname+'!E:E',
+//     }, (err, result) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             const cellValues = result.data.values;
+//             console.log(cellValues);
+//         }
+//     });
+// });
+
+app.post("/submit", async (req, res) => {
+    const fecha = new Date();
+    let { monto, tipo, paga, tipoPers } = req.body;
+    const sheetname = moment().format('MMMM');
+    console.log(req.body);
     if (paga === 'FC') { paga = ''; };
-    if (tipo === 'pers') {tipo = tipoPers}
-    if (tipo === 'INGRESO' ) {
+    if (tipo === 'pers') { tipo = tipoPers };
+    if (tipo === 'INGRESO') {
         await googleSheets.spreadsheets.values.append({
             auth,
             spreadsheetId,
-            range: sheetname+"!A:E",
+            range: sheetname + "!A:E",
             valueInputOption: "USER_ENTERED",
             resource: {
                 values: [[fecha.toLocaleDateString("en-GB"), tipo, monto, '', paga]],
@@ -51,14 +81,14 @@ app.post("/submit", async (req, res) => {
         await googleSheets.spreadsheets.values.append({
             auth,
             spreadsheetId,
-            range: sheetname+"!A:E",
+            range: sheetname + "!A:E",
             valueInputOption: "USER_ENTERED",
             resource: {
                 values: [[fecha.toLocaleDateString("en-GB"), tipo, '', monto, paga]],
             }
         })
     }
-    res.redirect('back');
+    res.status(200);
 });
 
 app.listen(process.env.PORT || 1337, (req, res) => console.log('wsup'));
